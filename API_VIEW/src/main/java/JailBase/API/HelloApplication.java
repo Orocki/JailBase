bf843798de9b692c069f77965def752114f86906
Dwayne5043 <96199669+Dwayne5043@users.noreply.github.com>
2023-12-22 21:22:04 +0100

    Update HelloApplication.java

--- API_VIEW/src/main/java/JailBase/API/HelloApplication.java
+++ API_VIEW/src/main/java/JailBase/API/HelloApplication.java
@@ -1,8 +1,9 @@
-package JailBase.API;
+package com.example.javafx_test;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,9 +12,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene1 = new Scene(fxmlLoader.load(), 751, 518);
         stage.setTitle("Hello!");
-        stage.setScene(scene);
+        stage.getIcons().add(new Image("https://th.bing.com/th/id/OIP.0QG5BDtlFMLl66Qe_UG1gAHaHr?rs=1&pid=ImgDetMain"));
+        stage.setScene(scene1);
         stage.show();
     }
 
