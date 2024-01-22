fb5afdb42772dd88e1b4751d5ec29c7147fa39c6
Dwayne5043 <96199669+Dwayne5043@users.noreply.github.com>
2024-01-22 22:16:45 +0100

    Create Main.java - Donik

---
+++ Jailbreak/Main.java
@@ -0,0 +1,12 @@
+package JailBreak;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Inmate inmate = new Inmate();
+        //Hier werden die IDs der Jails
+        inmate.fill(
+                "ky-hcj"
+        );
+    }
+}
